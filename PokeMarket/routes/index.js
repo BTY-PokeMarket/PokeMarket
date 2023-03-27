9914be5f9767fc637a070949c08ec125a7c95a04
justinnavarr0-ga <justinnavarr0@yahoo.com>
2023-03-27 12:04:30 -0700

    connect oauth and mongodb

--- PokeMarket/routes/index.js
+++ PokeMarket/routes/index.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
-const pokeCtrl = require('../controllers/pokemons')
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/pokemon')
 })
 
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
 
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/pokemon',
+    failureRedirect: '/pokemon'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/pokemon');
+  });
+});
 
 module.exports = router;
