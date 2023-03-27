21a0958a434c0ba0345a6762229be6feded5d21a
justinnavarr0-ga <justinnavarr0@yahoo.com>
2023-03-27 09:10:12 -0700

    added routes and aconnected PokeAPI to our app

--- PokeMarket/routes/index.js
+++ PokeMarket/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const ROOT_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get('/', async function (req, res, next) {
+    const poke = await fetch(`${ROOT_URL}`)
+    .then(res => res.json())
+  res.render('index', { poke });
+})
 module.exports = router;
