8e917fd782b87d7fb7c338c075d0a5dd5f7a6bbe
ryanqle <ryan.q.le@gmail.com>
2023-03-31 11:46:05 -0500

    remove console logs

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -92,8 +92,6 @@ async function deletePokedex (req,res) {
     const p1 = Pokedex.deleteOne({user: req.user})
     await User.updateOne({_id: req.user.id}, {$unset: {pokedex: 1}})
     let results = await Promise.all([p1]);
-    console.log(user)
-    console.log(results)
     res.redirect('/trainers')
    } catch (err) {
     console.log(err)
