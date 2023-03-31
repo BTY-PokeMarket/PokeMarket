6602bbda82d753439d531fb5132881ae591d226f
ryanqle <ryan.q.le@gmail.com>
2023-03-31 12:30:25 -0500

    Formatting and comments

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -3,71 +3,13 @@ const Pokedex = require('../models/pokedex')
 const Pokemon = require('../models/pokemon')
 const User = require('../models/user')
 
-
-async function deletePokemon(req, res) {
-    const pokedex = await Pokedex.findOne({ user: req.user.id });
-    const foundPokemon = await Pokemon.findOne({ dex: req.body.pokemonId })
-    thisPokemon = pokedex.pokemon
-    thisPokemon.remove(foundPokemon)
-    pokedex.save().then(() => {
-        res.redirect(`/pokedex/${pokedex._id}`)
-    }).catch(function (err) {
-        return next(err)
-    })
-}
-
-async function pokemonDetail(req, res) {
-    try {
-        const userPokedex = await Pokedex.findOne({ user: req.user.id });
-        const foundPokemon = await Pokemon.findById(req.params.id);
-        const pokemon = await fetch(`${ROOT_URL}-species/${foundPokemon.dex}`)
-            .then(res => res.json())
-        const sprite = await fetch(`${ROOT_URL}/${foundPokemon.dex}`)
-            .then(res => res.json())
-        res.render('pokedex/details', { pokemon, sprite, userPokedex, foundPokemon })
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-}
-
-async function showPokemons(req, res) {
-    const pokedex = await Pokedex.findById(req.params.id);
-    const availablePokemon = await Pokemon.find(
-        { _id: { $nin: pokedex.pokemon }, value: { $lte: pokedex.pokecoins } }
-      ).sort('dex');
-    res.render('pokedex/pokemon', { availablePokemon, pokedex })
-}
-
-async function addPokemon(req, res) {
-    try {
-        const pokedex = await Pokedex.findOne({ user: req.user.id });
-        const foundPokemon = await Pokemon.findOne({ dex: req.body.pokemonId })
-        const myPokedex = pokedex.pokemon
-        myPokedex.push(foundPokemon);
-        if(pokedex.pokecoins >= 0){
-            pokedex.pokecoins -= foundPokemon.value;
-            pokedex.totalCost += foundPokemon.value;
-        }
-        await pokedex.save();
-        res.redirect(`/pokedex/${pokedex._id}`)
-    } catch (err) {
-        console.log(err)
-    }
-}
-
-async function show(req, res, next) {
-    const pokedex = await Pokedex.findById(req.params.id).populate('pokemon');
-    const pokemon = await Pokemon.find();
-    const availablePokemon = await Pokedex.find({ pokemon });
-    res.render('pokedex/show', { pokedex, pokemon, availablePokemon })
-}
-
+// SHOW NEW POKEDEX FORM
 async function newPokedex(req, res) {
-    const pokedex = await Pokedex.findOne({ user: req.user.id });
     res.render('pokedex/new')
 }
 
+// CREATE POKEDEX
+// creating the pokedex with user inputted data
 async function create(req, res) {
     try {
         req.body.user = req.user._id;
@@ -86,36 +28,109 @@ async function create(req, res) {
     }
 }
 
-async function deletePokedex (req,res) {
-   try { 
-    const user = req.user;
-    const p1 = Pokedex.deleteOne({user: req.user})
-    await User.updateOne({_id: req.user.id}, {$unset: {pokedex: 1}})
-    let results = await Promise.all([p1]);
-    res.redirect('/trainers')
-   } catch (err) {
-    console.log(err)
-    res.redirect('pokedex/new');
+// DELETE POKEDEX
+// delete pokedex from database
+// removes the reference of the user's deleted pokedex from the user database
+async function deletePokedex(req, res) {
+    try {
+        const user = req.user;
+        const p1 = Pokedex.deleteOne({ user: req.user })
+        await User.updateOne({ _id: req.user.id }, { $unset: { pokedex: 1 } })
+        let results = await Promise.all([p1]);
+        res.redirect('/trainers')
+    } catch (err) {
+        console.log(err)
+        res.redirect('pokedex/new');
     }
 }
 
-async function edit (req, res) {
+// SHOW USER POKEDEX
+// populates the pokedex screen
+async function show(req, res, next) {
+    const pokedex = await Pokedex.findById(req.params.id).populate('pokemon');
+    const pokemon = await Pokemon.find();
+    const availablePokemon = await Pokedex.find({ pokemon });
+    res.render('pokedex/show', { pokedex, pokemon, availablePokemon })
+}
+
+// SHOW POKEMON DETAILS
+async function pokemonDetail(req, res) {
     try {
-    res.render('pokedex/edit')
+        const userPokedex = await Pokedex.findOne({ user: req.user.id });
+        const foundPokemon = await Pokemon.findById(req.params.id);
+        const pokemon = await fetch(`${ROOT_URL}-species/${foundPokemon.dex}`)
+            .then(res => res.json())
+        const sprite = await fetch(`${ROOT_URL}/${foundPokemon.dex}`)
+            .then(res => res.json())
+        res.render('pokedex/details', { pokemon, sprite, userPokedex, foundPokemon })
     } catch (err) {
-     console.log(err)
-    res.redirect('/pokemon');
+        console.log(err);
+        res.sendStatus(500);
     }
 }
 
-async function updatePokedexName (req, res) {
+// SHOW AVAILABLE POKEMON THAT USER CAN ADD
+async function showPokemons(req, res) {
+    const pokedex = await Pokedex.findById(req.params.id);
+    const availablePokemon = await Pokemon.find(
+        { _id: { $nin: pokedex.pokemon }, value: { $lte: pokedex.pokecoins } }
+    ).sort('dex');
+    res.render('pokedex/pokemon', { availablePokemon, pokedex })
+}
+
+// ADD POKEMON FROM DETAIL PAGE
+// Keep track of pokecoins used
+async function addPokemon(req, res) {
     try {
-    const pokedex = await Pokedex.findById(req.params.id)
-    pokedex.pokedexName = req.body.pokedexName;
-    await pokedex.save()
-    res.redirect(`/pokedex/${req.params.id}`)
+        const pokedex = await Pokedex.findOne({ user: req.user.id });
+        const foundPokemon = await Pokemon.findOne({ dex: req.body.pokemonId })
+        const myPokedex = pokedex.pokemon
+        myPokedex.push(foundPokemon);
+        if (pokedex.pokecoins >= 0) {
+            pokedex.pokecoins -= foundPokemon.value;
+            pokedex.totalCost += foundPokemon.value;
+        }
+        await pokedex.save();
+        res.redirect(`/pokedex/${pokedex._id}`)
     } catch (err) {
-    res.redirect('/pokemon');
+        console.log(err)
+    }
+}
+
+// DELETE POKEMON FROM POKEDEX
+async function deletePokemon(req, res) {
+    const pokedex = await Pokedex.findOne({ user: req.user.id });
+    const foundPokemon = await Pokemon.findOne({ dex: req.body.pokemonId })
+    thisPokemon = pokedex.pokemon
+    thisPokemon.remove(foundPokemon)
+    pokedex.save().then(() => {
+        res.redirect(`/pokedex/${pokedex._id}`)
+    }).catch(function (err) {
+        return next(err)
+    })
+}
+
+// EDIT SCREEN
+// render edit pokedex form
+async function edit(req, res) {
+    try {
+        res.render('pokedex/edit')
+    } catch (err) {
+        console.log(err)
+        res.redirect('/pokemon');
+    }
+}
+
+// UPDATE POKEDEX NAME
+// function updates the name of the pokedex
+async function updatePokedexName(req, res) {
+    try {
+        const pokedex = await Pokedex.findById(req.params.id)
+        pokedex.pokedexName = req.body.pokedexName;
+        await pokedex.save()
+        res.redirect(`/pokedex/${req.params.id}`)
+    } catch (err) {
+        res.redirect('/pokemon');
     }
 
 }
